4b1309f6613c8de2377487cb778ba4dd24c35402
pranavshukla1011 <shuklapranav1011@gmail.com>
2021-04-17 12:28:26 +0530

    Navbar

--- src/components/layout/AppLayout.js
+++ src/components/layout/AppLayout.js
@@ -2,5 +2,4 @@ import styled, { css } from 'styled-components';
 
 export default styled.div`
   padding: 40px;
-  border: 2px black solid;
 `;
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,18 +1,39 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+const LOGO = styled.div`
+  font-size: 1.5em;
+`;
 
 const NAVBAR = styled.div`
   display: grid;
   grid-template-columns: 180px auto 100px 100px;
 `;
 
+const ControlButtonElem = styled.div`
+  cursor: pointer;
+  ${(props) =>
+    props.active &&
+    css`
+      text-shadow: 0px 0px 20px #03ff03;
+    `}
+`;
+
+const toProperCase = (lower) => lower.charAt(0).toUpperCase() + lower.slice(1);
+
+const CONTROL_BUTTON = ({ name, active }) => {
+  return (
+    <ControlButtonElem active={active}>{toProperCase(name)}</ControlButtonElem>
+  );
+};
+
 const Navbar = () => {
   return (
     <NAVBAR>
-      <div>CD</div>
+      <LOGO>CD</LOGO>
       <div></div>
-      <div>Dashboard</div>
-      <div>Settings</div>
+      <CONTROL_BUTTON name='dashboard' active />
+      <CONTROL_BUTTON name='settings' />
     </NAVBAR>
   );
 };
